1db8cdf87fe5d928903c3ad75ee349b30df28ad8
Aayushi-Agarwal123 <aayushia971@gmail.com>
2026-07-21 15:34:07 +0530

    Initial portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen bg-[#050816] flex items-center justify-center">
+      <h1 className="text-6xl font-bold text-purple-500">
+        Portfolio Started 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
